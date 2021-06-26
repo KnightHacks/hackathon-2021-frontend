421eaf745e0f7cc1365513f4ea39fb2b29c19e94
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-06-25 20:29:02 -0400

    added paragraphs to the about text and changed the page title

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,16 +13,23 @@ const About = () => {
     <Page onLanding={false}>
       <div className="flex justify-start items-center w-full flex-col my-4 md:my-12">
         <h1 className="text-4xl sm:text-4xl mt-20 md:text-6xl text-center">
-          About Knights Hacks
+          About Us
         </h1>
         <div className="my-2 flex flex-col items-center w-2/3">
           <div className="text-left font-extralight sm:text-lg  mb-2 text-lg sm:text-lg md:text-xl lg:text-2xl mt-24">
-            Connect Collaborate and Create with 700 of the brightest developers,
-            engineers, and designers in the south-east. Whether you’re a
-            seasoned hacker or a tech newbie, Knight Hacks welcomes you. Just
-            bring an open mind and a insatiable desire to learn, and we’ll take
-            care of the rest. Create a product, learn new skills and have fun
-            with friends old and new – all in 36 hours.
+            <p>
+              Connect Collaborate and Create with 700 of the brightest developers,
+              engineers, and designers in the south-east. 
+            </p>
+            <p>
+              Whether you’re a seasoned hacker or a tech newbie, Knight Hacks 
+              welcomes you. Justbring an open mind and a insatiable desire to learn, 
+              and we’ll take care of the rest.
+            </p> 
+            <p>
+              Create a product, learn new skills and have fun
+              with friends old and new – all in 36 hours.
+            </p>
           </div>
         </div>
       </div>
