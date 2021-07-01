12d19e168b70063d9329454248f50e59a36d3fb2
Robert Boyd III <rob@robertcboyd.com>
2021-07-01 15:35:57 -0400

    Fix typo

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ const About = () => {
     <Page onLanding={false}>
       <div className="flex justify-start items-center w-full flex-col my-4 md:my-12">
         <h1 className="text-4xl sm:text-4xl mt-20 md:text-6xl text-center">
-          About Knights Hacks
+          About Knight Hacks
         </h1>
         <div className="my-2 flex flex-col items-center w-2/3">
           <div className="text-left font-extralight sm:text-lg  mb-2 text-lg sm:text-lg md:text-xl lg:text-2xl mt-24">
