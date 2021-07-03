c6808418a78fb2f611ebc741005fa8919960a5e7
Robert Boyd III <rob@robertcboyd.com>
2021-07-03 17:20:45 -0400

    Add createHacker()

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,4 +2,42 @@ const Register = () => {
   return <div>Register Page</div>;
 };
 
+/**
+ * @desc attempts to create a hacker on the backend
+ * @author Rob
+ */
+const createHacker = async (
+  first_name,
+  last_name,
+  school_name,
+  email,
+  phone_number,
+  resume,
+  track,
+  grad_year,
+  socials,
+  username,
+  password
+) => {
+  return fetch("/api/hackers/", {
+    method: "POST",
+    headers: { Accept: "application/json", "Content-Type": "application/json" },
+    body: JSON.stringify({
+      current_status: false,
+      date: new Date().toISOString(),
+      email,
+      first_name,
+      grad_year,
+      last_name,
+      password,
+      phone_number,
+      resume,
+      school_name,
+      socials,
+      tracks: [track],
+      username,
+    }),
+  });
+};
+
 export default Register;
