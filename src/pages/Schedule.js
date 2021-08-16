38fd18c01c1e6b4839eb632e7158a7d3ac288b60
Abraham Hernandez <abeherham@knights.ucf.edu>
2021-08-16 19:11:13 -0400

    Change onLanding value on Schedule page to false.

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -10,7 +10,7 @@ import ColorHash from "color-hash";
 const hash = new ColorHash({ lightness: 0.8 });
 const Schedule = () => {
   return (
-    <Page onLanding={true}>
+    <Page onLanding={false}>
       <div className="grid w-full flex-col my-4 md:my-12">
         <h1 className="my-10 justify-self-center text-4xl sm:text-5xl lg:text-6xl xl:text-7xl">
           Schedule
