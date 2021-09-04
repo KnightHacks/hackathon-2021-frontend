93934176cec6f4cefce45b77124d8db17ec387f0
Ro-Hanna Jowallah <rohannajowallah@gmail.com>
2021-09-03 20:42:06 -0400

    Implementing body

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -14,7 +14,7 @@ const Landing = () => {
       </Helmet>
       <Page onLanding={true}>
         <div className="flex justify-center items-center flex-col w-full mt-auto">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl w-full text-center">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl w-full text-center ">
             Welcome to
           </h1>
           <div className="bg-knight-hacks-logo w-full h-20 md:h-36 bg-no-repeat my-2 md:my-4 bg-center font-palanquin" />
