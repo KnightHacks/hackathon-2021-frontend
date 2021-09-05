14835f7b4967e7418ee9a5eb638f7840ed214a40
aileen <akendrap@gmail.com>
2021-09-04 22:06:31 -0400

    lag

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -42,7 +42,7 @@ const Menu = ({ open, setOpen }) => {
               enter="ease-out duration-300"
               enterFrom="opacity-0"
               enterTo="opacity-100"
-              leave="ease-in duration-700"
+              leave="ease-in duration-200"
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
@@ -72,7 +72,7 @@ const Menu = ({ open, setOpen }) => {
                 enter="ease-out duration-300"
                 enterFrom="opacity-0 scale-95"
                 enterTo="opacity-100 scale-100"
-                leave="ease-in duration-700"
+                leave="ease-in duration-200"
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
