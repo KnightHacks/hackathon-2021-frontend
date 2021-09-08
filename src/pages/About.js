38c1f71fe6dd541defb7bf9526a75bf0486c8dff
Ro-Hanna Jowallah <rohannajowallah@gmail.com>
2021-09-08 14:06:50 -0400

    Updated spacing

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,21 +16,21 @@ const About = () => {
           <h1 className="text-4xl sm:text-4xl mt-20 md:text-6xl text-center">
             About Us
           </h1>
-          <div className="my-2 flex flex-col items-center w-2/3 font-palanquin">
+          <div className="my-2 flex flex-col items-center w-2/3 ">
             <div className="text-left mb-2 text-lg sm:text-lg md:text-xl lg:text-2xl mt-24 font-palanquin">
-              <p>
-                Connect Collaborate and Create with 700 of the brightest
-                developers, engineers, and designers in the south-east.
-              </p>
-              <p>
-                Whether you’re a seasoned hacker or a tech newbie, Knight Hacks
-                welcomes you. Just bring an open mind and a insatiable desire to
-                learn, and we’ll take care of the rest.
-              </p>
-              <p>
-                Create a product, learn new skills and have fun with friends old
-                and new – all in 36 hours.
-              </p>
+              Connect Collaborate and Create with 700 of the brightest
+              developers, engineers, and designers in the south-east.
+              <br />
+              {""}
+              <br />
+              Whether you’re a seasoned hacker or a tech newbie, Knight Hacks
+              welcomes you. Just bring an open mind and a insatiable desire to
+              learn, and we’ll take care of the rest.
+              <br />
+              {""}
+              <br />
+              Create a product, learn new skills and have fun with friends old
+              and new – all in 36 hours.
             </div>
           </div>
         </div>
