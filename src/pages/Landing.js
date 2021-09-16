136d5febfb9252bfc7b29cd7883da646105e4c87
aileen <akendrap@gmail.com>
2021-09-15 23:18:32 -0400

    restyle twcss for readability

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -17,14 +17,24 @@ const Landing = () => {
           <h1 className="text-3xl md:text-4xl lg:text-5xl w-full text-center ">
             Welcome to
           </h1>
-          <div className="bg-knight-hacks-logo w-full h-20 md:h-36 bg-no-repeat my-2 md:my-4 bg-center font-palanquin" />
+          <div
+            className={`
+            bg-knight-hacks-logo
+            w-full h-20 md:h-36 bg-no-repeat
+            my-2 md:my-4 bg-center
+            font-palanquin"
+            `}
+          />
           <p className="text-xl w-full text-center font-palanquin">
             November 12th - November 14th, 2021
           </p>
           <Link
-            className={
-              "px-4 sm:px-6 md:px-12 py-2 border-white border-4 md:border-8 rounded-full text-xl sm:text-3xl md:text-4xl mt-4 md:mt-8 hover:bg-blue-400 focus:outline-none"
-            }
+            className={`
+              px-4 sm:px-6 md:px-12 py-2
+              border-white border-4 md:border-8
+              rounded-full text-xl sm:text-3xl md:text-4xl
+              mt-4 md:mt-8 hover:bg-blue-400 focus:outline-none
+            `}
             to="/register"
           >
             <p className="tracking-widest select-none" unselectable="on">
@@ -34,17 +44,25 @@ const Landing = () => {
           <div className="flex w-50% justify-between flex-col xl:flex-row">
             <a
               href="mailto:sponsorship@knighthacks.org"
-              className={
-                "font-palanquin px-4 sm:px-6 mx-5 md:px-12 py-2 border-white border-4 md:border-8 rounded-full text-xl sm:text-3xl md:text-4xl mt-4 md:mt-8 hover:bg-blue-400 focus:outline-none"
-              }
+              className={`
+                px-4 sm:px-6 mx-5 md:px-12 py-2
+                border-white border-4 md:border-8
+                rounded-full text-xl sm:text-3xl md:text-4xl
+                mt-4 md:mt-8 font-palanquin
+                hover:bg-blue-400 focus:outline-none
+              `}
             >
               <p>Sponsor Us</p>
             </a>
             <a
               href="https://discord.gg/CcZPUTFAuW"
-              className={
-                "font-palanquin px-4 sm:px-6 mx-5 md:px-12 py-2 border-white border-4 md:border-8 rounded-full text-xl sm:text-3xl md:text-4xl mt-4 md:mt-8 hover:bg-blue-400 focus:outline-none"
-              }
+              className={`
+                px-4 sm:px-6 mx-5 md:px-12 py-2
+                border-white border-4 md:border-8
+                rounded-full text-xl sm:text-3xl md:text-4xl
+                mt-4 md:mt-8 font-palanquin
+                hover:bg-blue-400 focus:outline-none
+              `}
             >
               <p>Join Discord</p>
             </a>
