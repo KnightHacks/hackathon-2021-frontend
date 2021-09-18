1fa73ebc3e984984384159c083edf3ddf347d4cb
aileen <akendrap@gmail.com>
2021-09-18 18:39:30 -0400

    update about message to Harold's suggestion

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,19 +13,20 @@ const About = () => {
         </h1>
         <div className="my-2 flex flex-col sm:text-base md:text-xl xl:text-xl items-center w-2/3 ">
           <div className="text-left space-x-4  mb-2 mt-16 font-palanquin">
-            Connect Collaborate and Create with 700 of the brightest developers,
-            engineers, and designers in the south-east.
+            Connect, Collaborate, and Create with over 700 of the brightest and
+            most enthusiastic developers, engineers, and designers in the
+            south-east and around the world!
             <br />
             {""}
             <br />
-            Whether you’re a seasoned hacker or a tech newbie, Knight Hacks
-            welcomes you. Just bring an open mind and a insatiable desire to
-            learn, and we’ll take care of the rest.
+            Whether {"you're"} a season hacker or a complete tech newbie, Knight
+            Hacks welcomes you! Just bring an open mind and a insatiable desire
+            to learn, and {"we'll"} take care of the rest.
             <br />
             {""}
             <br />
-            Create a product, learn new skills and have fun with friends old and
-            new – all in 36 hours.
+            Create a product, learn new skills, and have fun with friends old
+            and new - all in 36 hours!
           </div>
         </div>
       </div>
