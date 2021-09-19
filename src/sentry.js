63914c867b0b4dc9e97899cd27fd3684ef2f1bbf
Simon Weizman <contact@simon.weizman.us>
2021-09-19 11:44:24 -0400

    env variables need to be prefixed by REACT_APP_ bkz craco and cra

--- src/sentry.js
+++ src/sentry.js
@@ -3,15 +3,15 @@ import { Integrations } from "@sentry/tracing";
 
 export const setupSentry = (history) => {
   Sentry.init({
-    dsn: process.env.SENTRY_DSN,
+    dsn: process.env.REACT_APP_SENTRY_DSN,
     integrations: [
       new Integrations.BrowserTracing({
         tracingOrigins: ["api.knighthacks.org"],
         routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
       }),
     ],
-    environment: process.env.CF_PAGES_BRANCH ?? process.env.NODE_ENV,
-    release: process.env.CF_PAGES_COMMIT_SHA,
+    environment: process.env.REACT_APP_CF_PAGES_BRANCH ?? process.env.NODE_ENV,
+    release: process.env.REACT_APP_CF_PAGES_COMMIT_SHA,
     tracesSampleRate: 1.0,
     enabled: true,
     debug: true,
