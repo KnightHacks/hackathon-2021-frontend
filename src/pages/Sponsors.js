e24ef439402735e9bf33ddb28a1be2e17704150b
Ro-Hanna Jowallah <rohannajowallah@gmail.com>
2021-10-07 23:45:58 -0400

    Updated Break Points

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -62,14 +62,14 @@ const Sponsors = () => {
         />
       </div>
   */}
-      <div className="text-center sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl sm:mt-8 sm:mb-0 space-x-4 mt-8 mb-8 font-palanquin font-bold">
+      <div className="text-center sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl mb-4 space-x-4 mt-8  font-palanquin font-bold">
         GOLD
       </div>
 
       <div>_____________________________________</div>
       <div className="flex justify-center">
         <img
-          className="xl:w-96 lg:w-80 md:w-60 sm:w-36 sm:mr-7 sm:mt-8 mr-10 mb-"
+          className="xl:w-96 lg:w-80 md:w-60 w-44 sm:mr-7 sm:mt-8 mr-10 mb-"
           src={Facebook}
           alt="test logo"
         />
@@ -80,33 +80,33 @@ const Sponsors = () => {
       <div>_____________________________________</div>
       <div className="flex justify-center">
         <img
-          className="xl:w-96 lg:w-80 md:w-60 sm:w-44 sm:mr-7 sm:mt-8 mr-8"
+          className="xl:w-96 lg:w-80 md:w-60 w-44 sm:mr-7 sm:mt-8 mr-8"
           src={TI}
           alt="test logo"
         />
       </div>
-      <div className="text-center sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl sm:mt-8 sm:mb-.5 space-x-4  mb-2 font-palanquin font-bold">
+      <div className="text-center sm:text-xl mt-10 md:text-3xl lg:text-4xl xl:text-5xl sm:mt-8 sm:mb-.5 space-x-4  mb-2 font-palanquin font-bold">
         BRONZE
       </div>
       <div>_____________________________________</div>
       <div className="flex justify-center content-between ">
         <img
-          className=" xl:w-24 lg:w-20 lg:h-20  md:w-14 md:h-14 sm:w-10 sm:mr-7 sm:mt-8 mr-8 mb-14"
+          className=" xl:w-24 lg:w-20 lg:h-20 w-10 h-10 md:w-14 md:h-14 sm:w-10 sm:mr-7 sm:mt-8 mr-8 mb-14"
           src={EA}
           alt="test logo"
         />
         <img
-          className="xl:w-60 lg:mr-8 lg:w-44 md:w-28 sm:mt-8 sm:w-28 mr-8 mb-14 "
+          className="xl:w-60 lg:mr-8 lg:w-44 md:w-28 w-24 sm:mt-8 sm:w-28 mr-8 mb-14 "
           src={Google}
           alt="test logo"
         />
         <img
-          className="xl:w-60 lg:w-44 md:w-36 sm:mt-8 sm:w-28 sm:mr-8 mb-14 "
+          className="xl:w-60 lg:w-44 md:w-36 sm:mt-8 w-24 sm:w-28 sm:mr-8 mb-14 "
           src={Learner}
           alt="test logo"
         />
       </div>
-      <div className="text-center sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl  sm:mb-.5 space-x-4  mb-2 font-palanquin font-bold">
+      <div className="text-center sm:text-xl mt-4 md:text-3xl lg:text-4xl xl:text-5xl space-x-4 font-palanquin font-bold mt-8">
         PARTNER ORGANIZATIONS
       </div>
       <div>_____________________________________</div>
