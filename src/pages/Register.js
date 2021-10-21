00d54ac72588002f8a90eae572ec86008396892a
aileen <akendrap@gmail.com>
2021-10-20 22:42:29 -0400

    customize styling of dropdown indicator

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -66,7 +66,7 @@ const Register = () => {
   const DropdownIndicator = (props) => {
     return (
       <components.DropdownIndicator {...props}>
-        <SelectorIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
+        <SelectorIcon className="w-5 h-5 text-darkblue" aria-hidden="true" />
       </components.DropdownIndicator>
     );
   };
