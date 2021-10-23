7859369c77a80befc84da6ddc56b06c6f9530afa
aileen <akendrap@gmail.com>
2021-10-23 10:41:22 -0400

    include attributions page

---
+++ src/pages/Attributions.js
@@ -0,0 +1,88 @@
+import Page from "../components/Page";
+
+/**
+ * @desc Attributions Page
+ * @author Aileen
+ */
+const About = () => {
+  return (
+    <Page title="Knight Hacks | Attributions">
+      <div className="flex justify-start items-center w-full flex-col my-4 md:my-12">
+        <h1 className="text-4xl sm:text-4xl mt-20 md:text-6xl text-center font-sansita">
+          Attributions
+        </h1>
+        <div className="my-2 flex flex-col sm:text-base md:text-xl xl:text-xl items-center w-2/3 ">
+          <div className="text-left space-x-4  mb-2 mt-4 font-palanquin">
+            <h1 className="text-xl mt-16 mb-4 sm:text-xl md:text-3xl text-left font-sansita">
+              Special Thanks
+            </h1>
+            We want to extend a special thank you and provide credit to the
+            artists who made the music we&apos;ve used for this website.
+            <br />
+            -------------------------------
+            <br />
+            {""}
+            <br />
+            River Current By Back Pocket Sound
+            <br />
+            {""}
+            <br />
+            Chinese traditional musicå¤é£Žä¹æ›²--for loveç¼ ç»µæ‚±æ» free By
+            dragonfly & River Current By Back Pocket Sound mix
+            <br />
+            {""}
+            <br />
+            [Royalty Free Music] Atmospheric Relaxing Instrumental | Rainforest
+            By ROYALTY FREE MUSIC - DJ PURPL3 & River Current By Back Pocket
+            Sound mix
+            <br />
+            {""}
+            <br />
+            Meditation (demo music) By ArchAngel & River Current By Back Pocket
+            Sound mix
+            <br />
+            {""}
+            <br />
+            Relaxing piano and Water Sound [ NO COPYRIGHT MUSIC] By PetroVenus
+            <br />
+            {""}
+            <br />
+            Tokyo Chill Beats[ Chilofi & Nostalgic vibe Music ] CopyrightFree By
+            Tokyo Chill Beats & River Current By Back Pocket Sound mix
+            <br />
+            {""}
+            <br />
+            The Tokyo Business Hotel By explodecreative & River Current By Back
+            Pocket Sound mix
+            <br />
+            {""}
+            <br />
+            Still Awake by Ghostrifter bit.ly/ghostrifter-sc Creative Commons
+            â€” Attribution-ShareAlike 3.0 Unported â€” CC BY-SA 3.0 Free
+            Download: hypeddit.com/track/3vqwnk & River Current By Back Pocket
+            Sound mix
+            <br />
+            {""}
+            <br />
+            On My Way by Ghostrifter bit.ly/ghostrifter-sc Creative Commons â€”
+            Attribution-ShareAlike 3.0 Unported â€” CC BY-SA 3.0 Free Download:
+            hypeddit.com/track/lvf9kk & River Current By Back Pocket Sound mix
+            <br />
+            {""}
+            <br />
+            Mellow Out by Ghostrifter bit.ly/ghostrifter-sc Creative Commons â€”
+            Attribution-ShareAlike 3.0 Unported â€” CC BY-SA 3.0 Free Download:
+            hypeddit.com/track/cfrhzf & River Current By Back Pocket Sound mix
+            <br />
+            -------------------------------
+            <h1 className="text-xl mt-16 mb-4 sm:text-xl md:text-3xl text-left font-sansita">
+              Thank you!
+            </h1>
+          </div>
+        </div>
+      </div>
+    </Page>
+  );
+};
+
+export default About;
