3de4c0a8126e6c4310eadf4921e48881d128a097
aileen <akendrap@gmail.com>
2021-11-08 00:14:54 -0500

    update margins

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -28,7 +28,7 @@ const Landing = () => {
             November 12th - November 14th, 2021
           </p>
           <p className="mt-4 text-md w-full text-center font-sansitaitalic">
-            Registration is now closed. We welcome our 550+ hackers, mentors,
+            Registration is now closed. We welcome our 500+ hackers, mentors,
             and judges to Knight Hacks!
           </p>
           <div className="flex flex-col md:flex-row">
