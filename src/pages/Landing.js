3aa92adfc6829196f4fa4d561e63d4a0d7e51ddb
aileen <akendrap@gmail.com>
2021-11-12 12:35:24 -0500

    remove join discord button

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -34,41 +34,6 @@ const Landing = () => {
             Registration is now closed. We welcome our 600+ hackers, mentors,
             and judges to Knight Hacks!
           </p>
-          <div className="flex flex-col md:flex-row">
-            <a
-              href="https://discord.gg/CcZPUTFAuW"
-              className={`
-                relative group
-                overflow-hidden
-                px-4 py-2 md:mx-3 mt-4
-                border-darkblue border-4
-                rounded-xl text-2xl
-                font-palanquin
-                hover:text-purewhite
-                hover:bg-darkblue
-                hover:bg-opacity-70
-                hover:shadow-md
-                hover:bg-gradient-to-r
-                ease-out duration-300
-                focus:outline-none
-                focus:ring-4
-                focus:ring-darkblue
-                shadow-sm
-                dark:border-purewhite
-                dark:focus:ring-purewhite
-              `}
-            >
-              <span
-                className={`
-                absolute rounded-xl
-                right-0 w-8 h-32 -mt-12
-                transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12
-                group-hover:-translate-x-40 ease
-                `}
-              />
-              <p className="relative">Join Discord</p>
-            </a>
-          </div>
         </div>
       </Page>
     </>
